3c3390031f7e60d477145eaf8dabaec20ecaa1b7
Evan Jannot <ejannot@ensc.fr>
2023-08-25 14:06:40 -0400

    Create data scripts in backend and react app for frontend

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import works from './works'
+import brands from './brands'
+import about from './about'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [works, brands, about, experiences, skills, workExperience, contact]
